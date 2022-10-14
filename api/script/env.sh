ad9aa067f366e4ba9cb3fe9a08c99d34c59db9f9
seifchen <xuefeng1207@outlook.com>
2022-10-14 16:39:41 +0800

    env.sh
    env.sh

---
+++ api/script/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export KONG_PG_HOST=kong
+export KONG_PG_USER=kong
+export KONG_PG_DB=kong
+export KONG_PG_PORT=5432
+export KONG_PG_PWD=password
+export KONG_PROCESS_NUM=4
+export KONG_LOG_LEVEL=debug
